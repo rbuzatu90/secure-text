e450273112afb65cfadf16037f11dc231718dab2
Remus Buzatu <r.buzatu90@gmail.com>
2016-02-21 20:47:38 +0200

    Minor

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello Worldsdad!'
+    return 'Hello Worldy'
 
 
 @app.errorhandler(404)
